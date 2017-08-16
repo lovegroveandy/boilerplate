f9bf9047cddabfe8b2db0d27ae32fc7919583dca
lovegroveandy <andy@booodl.com>
2017-08-16 12:20:59 +1000

    action and reducer test

--- src/actions/title.spec.js
+++ src/actions/title.spec.js
@@ -3,11 +3,8 @@ import * as types from './types';
 import { setTitle } from './title';
 
 describe('The setTitle action', () => {
-  it('should return an object with type SET_TITLE', () => {
-    expect(setTitle()((a) => a).type).to.equal(types.SET_TITLE);
-  });
-  it('should pass its argument into the returned object with key title', () => {
-    const arg = 'some title';
-    expect(setTitle(arg)((a) => a).title).to.equal(arg);
-  });
+	it('should return a message with the type SET_TITLE', () => {
+		const result = setTitle({});
+		expect(result.type).to.equal(types.SET_TITLE)
+	})
 });
